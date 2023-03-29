00c31b22db93052f87121f8ba95ff99ae3693e9e
kazukishirasu <s19c1057fa@s.chibakoudai.jp>
2023-03-29 14:49:50 +0900

    push

--- src/run.cpp
+++ src/run.cpp
@@ -1,7 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
+#include "fstream"
+#include "yaml-cpp/yaml.h"
 
-class waypoint_nav2 : public rclcpp::Node {
+class WaypointNav2 : public rclcpp::Node {
 public:
-    
+    WaypointNav2() : Node("waypoint_nav2") {}
+    bool read_yaml() {
+        std::ifstream fin("/home/kazuki/ros2_ws/src/waypoint_nav2/waypoints/waypoint.yaml");
+        if (!fin) {
+            RCLCPP_ERROR(get_logger(), "Failed to open YAML file");
+            return false;
+        }
+        return true;
+    }
 private:
 };
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<WaypointNav2>());
+  rclcpp::shutdown();
+  return 0;
+}
